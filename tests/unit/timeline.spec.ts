50c0c28c587339f66c98d392f100d5d0c6ddb413
Oleksii Petrenko <oleksii.petrenko@valtech.com>
2022-06-06 21:38:07 +0300

    add tests for suspense

--- tests/unit/timeline.spec.ts
+++ tests/unit/timeline.spec.ts
@@ -1,22 +1,45 @@
 import {nextTick} from "vue";
-import {mount} from "@vue/test-utils";
+import {mount, flushPromises} from "@vue/test-utils";
 import TimeLine from "@/components/TimeLine.vue";
-import {today, thisWeek, thisMonth} from '@/mocks'
+import {today, thisWeek, thisMonth} from '@/mocks';
+
+jest.mock('axios', () => ({
+  get: (url: string) => {
+    return Promise.resolve({
+      data: [today, thisWeek, thisMonth]
+    })
+  }
+}))
+
+function mounTimeLine(){
+  return mount({
+    components: { TimeLine },
+    template: `
+      <suspense>
+        <template #default>
+          <time-line />
+        </template>
+        <template #fallback>
+          Loading...
+        </template>
+      </suspense>
+    `
+  });
+}
 
 describe('TimeLine', () => {
-  it('renders today post default', () => {
-    const wrapper = mount(TimeLine);
-
-    
+  it('renders today post default', async () => {
+    const wrapper = mounTimeLine();
+    await flushPromises();
     expect(wrapper.html()).toContain(today.created.format("Do MMM"));
   })
 
   // When click requestAnimationFram(() => ...) is called
   it('update when the period Week is clicked', async () => {
-    const wrapper = mount(TimeLine);
+    const wrapper = mounTimeLine();
     
     // wait for the next frame
-    // await nextTick();
+    await flushPromises();
     await wrapper.get('[data-test="This Week"]').trigger('click')
     expect(wrapper.html()).toContain(today.created.format("Do MMM"));
     expect(wrapper.html()).toContain(thisWeek.created.format("Do MMM"));
@@ -24,10 +47,10 @@ describe('TimeLine', () => {
 
   // When click requestAnimationFram(() => ...) is called
   it('update when the period Month is clicked', async () => {
-    const wrapper = mount(TimeLine);
-    
+    const wrapper = mounTimeLine();
     // wait for the next frame
     // await nextTick() or just add await before wrpper trigger
+    await flushPromises();
     await wrapper.get('[data-test="This Month"]').trigger('click')
     expect(wrapper.html()).toContain(today.created.format("Do MMM"));
     expect(wrapper.html()).toContain(thisWeek.created.format("Do MMM"));
